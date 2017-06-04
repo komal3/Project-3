2b960202e5c72513a4a7c099e9f98f2d8d3b1c48
komal jadvani <komaljadvani3@gmail.com>
2017-06-04 11:54:47 -0500

    Add Cast Feature, getting data in console but not able to get data on bootstrap modal box, trying to append data on modal

--- js/script.js
+++ js/script.js
@@ -76,6 +76,7 @@ function allMovie(){
           // '<a onclick="getTrailers(uniqueId)" href="'+onclick+'"'></a>
           '<a  id="'+uniqueId+'" onclick="getTrailors(this.id)" class="pop"  class="thumbnail"> <img src="' + image + '" /></a>',
           //'<a  onclick="getTrailors(responseId)" class="pop" id="trailer" class="thumbnail"> <img src="' + image + '" /></a>',
+          '<button type="button" id="'+uniqueId+'"  onclick="getCast(this.id)"  id="popup" class="btn btn-primary btn-lg btn-default">Cast Info</button>',
           '</li>'
         ].join(''));
           }
@@ -127,6 +128,68 @@ function getTrailors(responseId) {
   });
 }
 
+function getCast(responseId){
+
+  var id  = responseId
+    console.log('id:', responseId)
+  var apiurl = "http://api.themoviedb.org/3/movie/"+id+ "/casts?api_key=c1d2412e4b6a73bce280d933ff0dfc17";
+  var images = 'https://image.tmdb.org/t/p/w185';
+  $.ajax({
+    url: apiurl,
+    method: 'GET',
+    datatype: 'JSON',
+    data: {}
+  }).done(function(response){
+    console.log('response: ', response);
+    var casts = response.cast;
+
+
+    for (var i=0;  i<=5; i++) {
+      var picture = casts[i].profile_path;
+      var cast = casts[i].name;
+        console.log('cast:', cast);
+        var image = images + picture;
+
+        $('.modal-title').append(cast);
+        $('.modal-body').append('<img src="' + image + '" />');
+
+
+        $("#myModal").modal(show);
+
+
+    //     $('.popup').prepend([
+    //     '<div class="modal fade"  id="myModal" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">',
+    //       '<div class="modal-dialog">',
+    //
+    //
+    //         '<div class="modal-content">',
+    //           '<div class="modal-header">',
+    //             '<button type="button" class="close" data-dismiss="modal">&times;</button>',
+    //
+    //             '<h4 class="modal-title"  id="myModalLabel">'+ cast +'</h4>',
+    //           '</div>',
+    //           '<div class="modal-body">',
+    //              '<img src="' + image + '" class="imagepreview" style="width: 100%;" />',
+    //           '</div>',
+    //           '<div class="modal-footer">',
+    //             '<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>',
+    //           '</div>',
+    //         '</div>',
+    //
+    //     '  </div>',
+    //     '</div>'
+    // ].join(''));
+    }
+  //  window.open(popup, "_self");
+  })
+  .fail(function(error){
+    console.log('error', error);
+  })
+  .always(function(){
+    console.log("I always work");
+  })
+}
+
 function getMovie(movies){
   var apiUrl = 'https://api.themoviedb.org/3/search/movie?query=',
       movies,
@@ -165,6 +228,7 @@ function getMovie(movies){
           '<li class="col-sm-3">',
           '<h3>' + results + '</h3>',
           '<a  id="'+uniqueId+'" onclick="getTrailors(this.id)"  class="pop" id="trailer" class="thumbnail"> <img src="' + image + '" /></a>',
+          '<button type="button" id="'+uniqueId+'"  onclick="getCast(this.id)"  id="popup" class="btn btn-primary btn-lg btn-default">Cast Info</button>',
           '</li>'
         ].join(''));
           }
